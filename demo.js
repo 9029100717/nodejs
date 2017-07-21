137be249f60672771b15653a86dbb3c9b461e5e6
abhishek <as7340064@gmail.com>
2017-07-21 16:51:31 +0530

    create diff page

--- demo.js
+++ demo.js
@@ -5,7 +5,16 @@ app.get('/',function(req,res){
     res.send("hello world");
 });
 
-app.listen(3001,function(){
+app.get('/users',function(req,res){
+    console.log("got a get request for page user");
+    res.send("user page");
+});
+
+app.get('/list', function(req,res){
+    console.log("got a request for list");
+    res.send("user list");
+});
+app.listen(2000,function(){
     console.log("server started....");
 
 });
