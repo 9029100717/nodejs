adbaf7f448ee118d4bedf209ed5c06913567f607
abhishek <as7340064@gmail.com>
2017-07-24 14:57:27 +0530

    callback function

--- demo.js
+++ demo.js
@@ -1,21 +1,34 @@
-var express=require('express');
-var app=express();
+//var express=require('express');
+//var app=express();
 
-app.get('/',function(req,res){
-    res.send("hello world");
-});
+//app.get('/',function(req,res){
+  //  res.send("hello world");
+//});
 
-app.get('/users',function(req,res){
-    console.log("got a get request for page user");
-    res.send("user page");
-});
+//app.get('/users',function(req,res){
+  //  console.log("got a get request for page user");
+    //res.send("user page");
+//});
 
 
-app.get('/:id',function(req,res){
-    console.log("the id you specified is:");
-    res.send('the id you specified is:'+ req.parans.id);
-});
-app.listen(2000,function(){
-    console.log("server started...");
+//app.get('/:id',function(req,res){
+  //  console.log("the id you specified is:");
+    //res.send('the id you specified is:'+ req.parans.id);
+//});
+//app.listen(2000,function(){
+  //  console.log("server started...");
 
-});
+//});
+
+console.log("user 1 send the request to the database");
+setTimeout(callback,5000);
+
+console.log("user 2 send the request to the database");
+setTimeout(callback,5000);
+
+console.log("user 3 send request to the database");
+setTimeout(callback,5000);
+
+function callback(){
+    console.log("queried the database and delever the data in 5 second");
+}
