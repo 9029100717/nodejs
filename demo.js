8f2533390efaf2c6b6760ff3a59c13eb19ed9b50
abhishek <as7340064@gmail.com>
2017-07-24 15:32:51 +0530

    Lambda function

--- demo.js
+++ demo.js
@@ -20,6 +20,8 @@
 
 //});
 
+//callback function
+
 console.log("user 1 send the request to the database");
 setTimeout(callback,5000);
 
@@ -31,4 +33,10 @@ setTimeout(callback,5000);
 
 function callback(){
     console.log("queried the database and delever the data in 5 second");
+}
+
+exports.myhandeler=fuction(event, context, callback){
+    console.log("value1=" + event.key1);
+    console.log("value2=" + event.key2);
+    callback(null, "some success message");
 }
